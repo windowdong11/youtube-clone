5912d3a15557a9f68737c92f030982775a0e4bea
windowdong11 <windowdong11@naver.com>
2022-07-01 15:47:57 +0900

    프론트엔드 auth0 인증 확인 예시 작성

--- youtube-clone-front/src/App.tsx
+++ youtube-clone-front/src/App.tsx
@@ -1,26 +1,131 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+import { useAuth0 } from "@auth0/auth0-react";
+
+// styles
+import "./App.css";
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+};
+
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={() => logout({ returnTo: window.location.origin })}>
+      Log Out
+    </button>
+  );
+};
+
+const Profile = () => {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading || user === undefined) return <div>Loading ...</div>;
+
+  if(!isAuthenticated) return <div>Auth required</div>
+  
+  return (
+    <div>
+      <img src={user.picture} alt={user.name} />
+      <h2>{user.name}</h2>
+      <p>{user.email}</p>
+    </div>
+  )
+};
+
+const Posts = () => {
+  const { getAccessTokenSilently } = useAuth0();
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const token = await getAccessTokenSilently({
+          audience: 'https://youtube-clone.jp.auth0.com/api/v2/',
+          scope: 'read:messages',
+        });
+        console.log(`Bearer ${token}`);
+        const response = await fetch('http://localhost:8000/api/private', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          method: 'GET',
+        });
+        setPosts((await response.json()).message);
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, [getAccessTokenSilently]);
+
+  if (!posts) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>{posts}</div>
+  );
+};
+
+const Public = () => {
+  const [result, setResult] = useState<string>();
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch('http://localhost:8000/api/public', {
+          method: 'GET',
+        });
+        setResult((await response.json()).message);
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, []);
+  console.log(result)
+
+  if(!result) return <div>Public : Loading...</div>;
+
+  return <div>Public : {result}</div>
+}
+
+const App = () => {
+  const { isLoading, error, isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const [accessToken, setAccessToken] = useState<string>();
+  useEffect(() => {
+    async function sat() {
+      setAccessToken(await getAccessTokenSilently());
+    }
+    sat()
+  }, [isAuthenticated])
+
+  if (error) {
+    return <div>Oops... {error.message}</div>;
+  }
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+  if (!isAuthenticated) {
+    return <div>
+      <Public/>
+      Authenticated : {accessToken}
+      <LoginButton/>
+      </div>
+  }
+
+  return (
+    <div>
+      <Public/>
+      Authenticated
+      <Profile/>
+      <Posts/>
+      <LogoutButton/>
     </div>
   );
-}
+};
 
 export default App;
--- youtube-clone-front/src/index.tsx
+++ youtube-clone-front/src/index.tsx
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="youtube-clone.jp.auth0.com"
+    clientId="JPLfbYEORPLzQnMZhVhOL4M71Zy0tJf7"
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
