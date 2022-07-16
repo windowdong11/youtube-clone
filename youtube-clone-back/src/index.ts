bbb8f96d9e22ec4ad46abab54e278033b14b5d0d
windowdong11 <windowdong11@naver.com>
2022-07-17 03:34:30 +0900

    BE : CORS 허용 주소 환경변수에 주어진 값으로 변경

--- youtube-clone-back/src/index.ts
+++ youtube-clone-back/src/index.ts
@@ -26,7 +26,7 @@ const app = express()
 
 app.use(express.json())
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://windowdong11.ga'); // Allow CORS, React App URL
+  res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT); // Allow CORS, React App URL
   res.setHeader('Access-Control-Allow-Methods', ['GET', 'POST', 'PUT', 'DELETE']);
   res.setHeader('Access-Control-Allow-Headers', 'authorization');
   next();
