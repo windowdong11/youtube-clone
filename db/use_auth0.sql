79c25b686d3275b5ae9e52825485719201b0babe
windowdong11 <windowdong11@naver.com>
2022-07-16 23:47:50 +0900

    DB: Add cascade option to auth0_user table

--- db/use_auth0.sql
+++ db/use_auth0.sql
@@ -1,5 +1,5 @@
 ALTER TABLE website_user DROP password_hash;
 CREATE TABLE auth0_user (
-  user_id integer NOT NULL REFERENCES website_user(user_id),
+  user_id integer NOT NULL REFERENCES website_user(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
   auth0_id text NOT NULL
 );
